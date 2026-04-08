8ce08c89a8268d0afb72ea5976c44423f179429f
user2988 <266582373+user2988@users.noreply.github.com>
2026-04-08 13:36:22 -0400

    Update cortex.py

--- cortex.py
+++ cortex.py
@@ -220,13 +220,13 @@ class FitbitClient:
         data    = self._get(f"/1/user/-/activities/date/{d}.json")
         summary = data.get("summary", {})
 
+        # DEBUG
         print("----- DEBUG HEART RATE -----")
         print("DATE:", d)
         print("HR VALUE:", value)
         print("----------------------------")
 
-        return {"resting_heart_rate": value.get("restingHeartRate")}
-        
+        return {"resting_heart_rate": value.get("restingHeartRate")}        
         return {
             "steps":                   summary.get("steps"),
             "calories_out":            summary.get("caloriesOut"),
